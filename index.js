9408501cfa7a42126bb2de2a267de8d5328225c1
Brian Wangora <brianwangora@gmail.com>
2022-06-08 20:53:46 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = ('bob');
+var upperCaseCustomerName = function () {
+    return customerName = ('BOB')
+}
+function setBestCustomer  () {
+   return bestCustomer = ('not bob')
+}
+function overwriteBestCustomer () {
+    return bestCustomer = ('maybe bob')
+}
+const leastFavoriteCustomer = ('Ken')
+function changeLeastFavoriteCustomer () {
+    return leastFavoriteCustomer = ('Jane')
+}
